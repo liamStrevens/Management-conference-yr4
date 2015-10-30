4df609da3dc5a691f61e7e6263a14ccfe5bb40ad
liam strevens <liamstrevens131@hotmail.com>
2015-10-30 22:16:34 +0000

    start of participart in conference

---
+++ Management conference/Gui/ConferenceForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Management_conference.Logic;
+using Management_conference.Gui;
+using Management_conference;
+namespace Management_conference.Gui
+{
+    public partial class ConferenceForm : Form
+    {
+        Conference thisConference;
+        public ConferenceForm(Conference selectedConf)
+        {
+            thisConference = selectedConf;
+            InitializeComponent();
+            
+        
+        }
+        
+    }
+}
